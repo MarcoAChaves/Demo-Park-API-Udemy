abb0d94fbd7815ab1bddf8929943b8fed0c742d2
Marco Chaves <macschaves@gmail.com>
2026-03-11 09:44:08 -0300

    Documentação e Testes do sistema de autenticação

--- demo-park-api/src/test/java/com/chaves/demo_park_api/UsuarioIT.java
+++ demo-park-api/src/test/java/com/chaves/demo_park_api/UsuarioIT.java
@@ -6,10 +6,8 @@ import com.chaves.demo_park_api.web.dto.UsuarioSenhaDto;
 import com.chaves.demo_park_api.web.exception.ErrorMessage;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
-import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.web.reactive.server.WebTestClient;
 
