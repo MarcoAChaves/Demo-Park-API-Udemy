365ebb72c5aee7fcb303e4102ceca650eeebefd1
Marco Chaves <macschaves@gmail.com>
2026-03-11 16:55:28 -0300

    Recursos de Clientes

---
+++ demo-park-api/src/main/java/com/chaves/demo_park_api/web/dto/mapper/PageableMapper.java
@@ -0,0 +1,17 @@
+package com.chaves.demo_park_api.web.dto.mapper;
+
+
+import com.chaves.demo_park_api.web.dto.PageableDto;
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+import org.modelmapper.ModelMapper;
+import org.springframework.data.domain.Page;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class PageableMapper {
+
+    public static PageableDto toDto(Page page) {
+        return new ModelMapper().map(page, PageableDto.class);
+    }
+}
+
--- demo-park-api/src/main/java/com/chaves/demo_park_api/web/exception/ApiExceptionHandler.java
+++ demo-park-api/src/main/java/com/chaves/demo_park_api/web/exception/ApiExceptionHandler.java
@@ -1,5 +1,6 @@
 package com.chaves.demo_park_api.web.exception;
 
+import com.chaves.demo_park_api.exception.CpfUniqueViolationException;
 import com.chaves.demo_park_api.exception.EntityNotFoundException;
 import com.chaves.demo_park_api.exception.PasswordInvalidException;
 import com.chaves.demo_park_api.exception.UsernameUniqueViolationException;
@@ -48,7 +49,7 @@ public class ApiExceptionHandler {
                 .body(new ErrorMessage(request, HttpStatus.NOT_FOUND, ex.getMessage()));
     }
 
-    @ExceptionHandler(UsernameUniqueViolationException.class)
+    @ExceptionHandler({UsernameUniqueViolationException.class, CpfUniqueViolationException.class})
     public ResponseEntity<ErrorMessage>uniqueViolationException(RuntimeException ex,
                                                                        HttpServletRequest request){
         log.error("Api Error - ", ex);
